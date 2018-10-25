8217b73c83541fa293986f0b850a8f26be87d8af
Corina Talavera <corina.talavera@gmail.com>
2018-10-25 22:37:47 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
-while levitation_force < 6
-  puts "The current number is less than 20."
+while levitation_force < 1-
+  puts "Wingardium Leviosa"
   counter += 1
 end    
 end
